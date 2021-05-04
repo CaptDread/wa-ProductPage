9febd3c3548b0c8539867bf97ec2436b2503f262
CaptDread <domlycaon@gmail.com>
2021-05-04 13:30:32 -0400

    rotatingWheelWork
    started working on the rotating wheel

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,26 @@
 "use strict";
 
-console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+var packBtn = document.querySelectorAll('.package_btn');
+
+var _loop = function _loop(s) {
+  packBtn[s].addEventListener('click', function () {
+    var activeBuild = document.querySelector('#active_build');
+    var activeBtn = document.querySelector("#active_btn");
+    var vanBuildImg = document.querySelectorAll(".van_build-img");
+    var packageLabel = document.querySelectorAll('.package label img');
+    activeBuild.id = '';
+    vanBuildImg[s + 1].setAttribute('id', 'active_build');
+
+    if (activeBtn != null) {
+      activeBtn.id = '';
+      packageLabel[s].setAttribute('id', 'active_btn');
+    } else {
+      packageLabel[s].setAttribute('id', 'active_btn');
+    }
+  });
+};
+
+for (var s = 0; s < packBtn.length; s++) {
+  _loop(s);
+}
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
-console.log(`Hello World from main.js! 
-Change this message, and make sure it changes in the browser 
-to verify that you're working in the right files.`);
+let packBtn = document.querySelectorAll('.package_btn')
+
+for (let s = 0; s < packBtn.length; s++) {
+    packBtn[s].addEventListener('click', function() {
+        let activeBuild = document.querySelector('#active_build')
+        let activeBtn = document.querySelector(`#active_btn`)
+        let vanBuildImg = document.querySelectorAll(`.van_build-img`)
+        let packageLabel = document.querySelectorAll('.package label img')
+        activeBuild.id = ''
+        vanBuildImg[s + 1].setAttribute('id', 'active_build')
+        if (activeBtn != null) {
+            activeBtn.id = ''
+            packageLabel[s].setAttribute('id', 'active_btn')
+        } else {
+            packageLabel[s].setAttribute('id', 'active_btn')
+
+        }
+
+    })
+}
